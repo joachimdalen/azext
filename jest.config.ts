267d94675bcd5ae0cab765534e4fdaf85067332a
Joachim Dalen <me@d3v.no>
2021-12-16 07:19:26 +0100

    test: Add coverage

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,10 @@ const config: Config.InitialOptions = {
   verbose: true,
   transform: {
     '^.+\\.ts?$': 'ts-jest'
-  }
+  },
+  collectCoverageFrom: ['src/**/*.ts', '!src/azext.ts'],
+  testEnvironment: 'node',
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts)$',
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules']
 };
 export default config;
